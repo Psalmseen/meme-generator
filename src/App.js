76f1f0bb0d0fea36300e591777d6db1162f83637
Psalmseen <55616929+Psalmseen@users.noreply.github.com>
2021-08-20 22:53:23 +0100

    Updated app  to use react hooks

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state= {
-            topText:"",
-            bottomText:"",
-            memeImg:"https://i.imgflip.com/9ehk.jpg",
-            memeCollection: []
+const App = () => {
+  const [state, setState] = useState({
+    topText: "",
+    bottomText: "",
+    memeImg: "https://i.imgflip.com/9ehk.jpg",
+    memeCollection: [],
+  });
 
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    handleSubmit(event){
-        event.preventDefault()
-        const randImg = this.state.memeCollection[Math.floor(Math.random() * this.state.memeCollection.length)].url
-        this.setState({memeImg : randImg})
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const randImg =
+      state.memeCollection[
+        Math.floor(Math.random() * state.memeCollection.length)
+      ].url;
+    setState({ ...state, memeImg: randImg });
+  };
 
-    }
-    handleChange(event){
-        const {name, value} = event.target
-        this.setState({[name]: value})
-        console.log(this.state.memeCollection)
-    }
-    componentDidMount(){
-        fetch("https://api.imgflip.com/get_memes")
-            .then(response => response.json())
-            .then(data => this.setState({memeCollection : data.data.memes}))
-    }
-     render(){
-         return(
-             <div>
-                 <form onSubmit={this.handleSubmit}>
-                     <input name="topText" onChange={this.handleChange} value={this.state.topText} placeholder="Enter the top text" />
-                     <input name="bottomText" onChange={this.handleChange} value={this.state.bottomText} placeholder="Enter the bottom text" />
-                     <button>Get Meme</button>
-                 </form>
-                <div className="wrapper">
-                    <div>
-                        <img src={this.state.memeImg} alt="meme" className="meme-image"/>
-                    </div>
-                    <div className="meme-container">
-                        <h1 className="top-text"> {this.state.topText}</h1>
-                        <h1 className="bottom-text">{this.state.bottomText}</h1>
-                    </div>
-                </div>
-             </div>
-         )
-     }
-}
+  const print = () => {
+    window.print();
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState({ ...state, [name]: value });
+  };
 
-export default App
+  useEffect(() => {
+    const fetchImage = async () => {
+      const resp = await fetch("https://api.imgflip.com/get_memes");
+      const data = await resp.json();
+      setState({ ...state, memeCollection: data.data.memes });
+    };
+    fetchImage();
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="topText"
+          onChange={handleChange}
+          value={state.topText}
+          placeholder="Enter the top text"
+        />
+        <input
+          name="bottomText"
+          onChange={handleChange}
+          value={state.bottomText}
+          placeholder="Enter the bottom text"
+        />
+        <button>Get Meme</button>
+      </form>
+      <div className="wrapper">
+        <div>
+          <img src={state.memeImg} alt="meme" className="meme-image" />
+        </div>
+        <div className="meme-container">
+          <h1 className="top-text"> {state.topText}</h1>
+          <h1 className="bottom-text">{state.bottomText}</h1>
+        </div>
+      </div>
+      <button onClick={() => print()}> Print</button>
+    </div>
+  );
+};
+
+export default App;
